1b397e8daaf5cbb7684c5a362f6baa98d47adbd2
iheathdesigns <ianheath2010@gmail.com>
2023-01-15 07:45:48 -0500

    Initial commit

---
+++ DefiningFields/src/Fields.java
@@ -0,0 +1,19 @@
+
+public class Fields {
+
+	public static void main(String[] args) {
+		String firstName = "Ian";
+		System.out.println(firstName);
+		
+		//Illegal fields
+		Int x = 34.45;
+		Float x2 = 80;
+		Boolean a = "Number";
+		
+		String lastName;
+		Int n;
+		Float x3;
+		Boolean b;
+	}
+
+}
